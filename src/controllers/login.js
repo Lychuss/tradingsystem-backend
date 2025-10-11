fcab384dcc5c6055878d199e88f05c10ff6bafdd
Raphae San Juan <raphaelsanjuan6@gmail.com>
2025-10-11 22:23:09 +0800

    send the student_id in the frontend

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { registration } from "../middlewares/authentication.js";
-import { generateToken } from "../services/logics.js";
+import { generateToken, getStudentsId } from "../services/logics.js";
 
 const loginRouter = express.Router();
 
@@ -12,9 +12,10 @@ loginRouter.post('/yes4trade/auth/login',registration , async (req, res) => {
     const { username } = req.body;
     try {
         const token = await generateToken(username);
+        const student_id = await getStudentsId(username);
 
         if(token){
-            return res.status(200).json({ token: token });
+            return res.status(200).json({ token: token, student_id: student_id });
         }
             return res.status(400).json({ message: 'Error account not valid!'});
 
